70336a31f36b51de6b1fff3e27583ba74351bfbe
ovrudenko <zlobny_like@rambler.ru>
2013-11-18 22:28:43 +0400

    10 - попытка сделать модальное окно

--- Agromir/web/resources/scripts/auth.js
+++ Agromir/web/resources/scripts/auth.js
@@ -3,7 +3,29 @@
  * and open the template in the editor.
  */
 
-function auth(){
-    $('.login').lightbox_me();
-}
+$(document).ready(function(){
+    $('#enter div').hide();
+    $('#qqq').toggle(function() {
+        $(this)
+        .addClass('active')
+        .next('#login')
+        .animate({
+            'height':'show'
+        }, {
+            duration: 'slow',
+            easing: 'swing'
+        });
+    }, function() {
+        $(this)
+        .removeClass('active')
+        .next('#login')
+        .slideUp();
+    });
+    $('#login :submit').click(function() {
+        $(this)
+        .parent()
+        .prev('#qqq')
+        .click();
+    });
+});
 
